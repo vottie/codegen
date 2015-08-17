bfb32422632325ee61e8e2df7ba475d4692c9786
vottie <tsuboi23@gmail.com>
2015-08-18 01:34:57 +0900

    Added the processing for generate shared library.

--- codegen.sh
+++ codegen.sh
@@ -30,6 +30,39 @@ EOT
 }
 
 #
+# create Makefile for shared library
+#
+create_lib_makefile()
+{
+cat << EOT
+CXX=g++
+CXXFLAGS= -O0 -Wall -g -fPIC
+LDFLAGS= 
+TARGET=lib$SONAME.so
+TEST_TARGET=main
+
+OPT= 
+INC=
+LIB=
+
+SRC=$CLASS.cpp
+HEAD=$CLASS.h
+OBJ=\$(SRC:. cpp=.o)
+
+all: \$(TARGET)
+
+\$(TARGET): \$(OBJ)
+	\$(CXX) \$(CXXFLAGS) -shared -o \$(TARGET) \$(OBJ) \$(LIB)
+
+clean:
+	\$(RM) \$(TARGET) *.o
+
+test:
+	\$(CXX) main.cpp \$(CXXFLAGS) -o \$(TEST_TARGET) -I. -L\$(CURDIR) -l$SONAME
+EOT
+}
+
+#
 # create cpp file
 #
 create_cpp()
@@ -91,18 +124,78 @@ EOT
 #
 # main processing of shell script
 #
-if [ "$#" != 3 ]; then
-    echo "USAGE:"
-    echo "codegen.sh project namespace class"
-    exit
+case "$1" in
+    "c")
+        echo "build c project"
+        TYPE=CLANG
+        ;;
+    "cpp")
+        echo "build c++ project"
+        TYPE=CPP
+        ;;
+    *)
+        echo "error"
+        exit
+        ;;
+esac
+
+if [ $TYPE = "CLANG" ]; then
+    PROJECT=$2
+    if [ $# != 3 ]; then
+        echo "USAGE:"
+        echo "codegen.sh c project file"
+        exit
+    fi
+    FILE=$3
+    echo "$PROJECT"
+    echo "$FILE"
+else 
+    if [ $# -eq 4 ]; then
+        PROJECT=$2
+        NAMESPACE=$3
+        CLASS=$4
+        echo "$PROJECT"
+        echo "$NAMESPACE"
+        echo "$CLASS"
+
+        mkdir $PROJECT
+        create_makefile $PROJECT > $PROJECT/Makefile
+        create_cpp $NAMESPACE > $PROJECT/$CLASS.cpp
+        create_h $NAMESPACE $CLASS > $PROJECT/$CLASS.h
+        create_main  $NAMESPACE > $PROJECT/main.cpp
+    elif [ $# -eq 6 ]; then
+        case "$2" in
+            "test")
+                echo "test"
+                #            exit
+                ;;
+            "lib")
+                echo "lib"
+                #            exit
+                ;;
+            *)
+                echo "error"
+                exit
+                ;;
+        esac
+        PROJECT=$3
+        NAMESPACE=$4
+        CLASS=$5
+        SONAME=$6
+        
+        echo "$PROJECT"
+        echo "$NAMESPACE"
+        echo "$CLASS"
+        echo "$SONAME"
+
+        mkdir $PROJECT
+        create_lib_makefile $PROJECT > $PROJECT/Makefile
+        create_cpp $NAMESPACE > $PROJECT/$CLASS.cpp
+        create_h $NAMESPACE $CLASS > $PROJECT/$CLASS.h
+        create_main  $NAMESPACE > $PROJECT/main.cpp
+    else
+        echo "invalid argument"
+        echo $#
+    fi
 fi
 
-PROJECT=$1
-NAMESPACE=$2
-CLASS=$3
-
-mkdir $PROJECT
-create_makefile $PROJECT > $PROJECT/Makefile
-create_cpp $NAMESPACE > $PROJECT/$CLASS.cpp
-create_h $NAMESPACE $CLASS > $PROJECT/$CLASS.h
-create_main  $NAMESPACE > $PROJECT/main.cpp
